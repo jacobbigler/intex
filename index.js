b3f4c9f3070f82d28f6eb6c916d56f411aae977d
jacobbigler <jacob.bigler03@gmail.com>
2023-12-05 15:08:27 -0700

    Did report.ejs and survey.ejs functionality
    editUser does not work yet. report.ejs pulls all information from the database and displays it. survey.ejs takes all information from inputs and adds it to the database

--- index.js
+++ index.js
@@ -48,16 +48,70 @@ const knex = require("knex")({
 
 //GET requests below:
 
-app.get("/survey", (req, res) => {
+app.get("/report", (req, res) => {
     knex.select().from("user-input").then( userInput => {
-        res.render("survey", { myuser : userInput });
+        res.render("report", { myuser : userInput });
     });
 });
 
 
 app.post('/survey', (req, res) => {
-    knex('TABLE NAME').insert({
-        age:
-        req.body.age
-    })
-})
+    knex('smu-mh').insert({
+        age: req.body.age ,
+        gender: req.body.gender ,
+        relationship_status: req.body.relationship ,
+        occupation_status: req.body.occupation ,
+        organization_affiliation: req.body.organizations ,
+        social_media_use : req.body.use_social_media ,
+        social_media_platform: req.body.social_media_platforms ,
+        time_usage: req.body.time_spent ,
+        use_without_purpose: req.body.use_without_purpose ,
+        distracted_by_social_media: req.body.distracted_by_social_media ,
+        restless_without_social_media: req.body.restless_without_social_media ,
+        easily_distracted: req.body.easily_distracted ,
+        bothered_by_worries: req.body.bothered_by_worries ,
+        concentration_difficulty: req.body.concentration_difficulty ,
+        compare_self_to_others: req.body.compare_self_to_others ,
+        opinions_about_comparison: req.body.opinions_about_comparison ,
+        seek_validation: req.body.seek_validation ,
+        feel_depressed: req.body.feel_depressed ,
+        daily_activity_interest_fluctuations: req.body.daily_activity_interest_fluctuations ,
+        sleep_issues: req.body.sleep_issues        
+}).then(myuser => {
+    res.redirect("/");
+});
+});
+
+app.get("/editUser/:id", (req, res)=> {
+    knex.select("u.city",
+          "u.age",
+          "u.gender",
+          "u.relationship_status",
+          "u.occupation_status",
+          "oa.organization_affiliation",
+          "u.social_media_use",
+          "smp.social_media_platform",
+          "u.time_usage",
+          "r.use_without_purpose",
+          "r.restless_without_social_media",
+          "r.distracted_by_social_media",
+          "r.easily_distracted",
+          "r.bothered_by_worries",
+          "r.concentration_difficulty",
+          "r.compare_self_to_others",
+          "r.opinions_about_comparison",
+          "r.seek_validation",
+          "r.feel_depressed",
+          "r.daily_activity_interest_fluctuations",
+          "r.sleep_issues")
+          .from({u: "user_inputs" })
+          .join({r: "ratings"}, "u.user_id", "=", "r.user_id")
+          .join({smp: "social_media_platforms"},  "u.user_id", "=", "smp.user_id")
+          .join({oa: "organization_affiliations"},  "u.user_id", "=", "oa.user_id")
+          .where("u.user_id", req.params.id).then(userInput => {
+    res.render("editUser", {myuser: userInput});
+   }).catch( err => {
+      console.log(err);
+      res.status(500).json({err});
+   });
+ })
