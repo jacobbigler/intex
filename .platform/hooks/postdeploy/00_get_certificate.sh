7ac6dda1d53c3b92e5218f135d9ef941f9507045
jacobbigler <jacob.bigler03@gmail.com>
2023-12-07 09:08:53 -0700

    Update 00_get_certificate.sh
    changed the https to the a dns record

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d intex-311.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email jacob.bigler03@gmail.com
+sudo certbot -n -d intex311.is404.net --nginx --agree-tos --email jacob.bigler03@gmail.com
